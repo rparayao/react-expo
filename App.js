c5d04e52be675c0adec062389b485d5d27700a1e
Parayao <B14850@NGC.AUTHORITY>
2020-05-30 23:38:19 -0700

    update to make it work

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import Main from './app/Main';
+import React,{ Component }  from 'react';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -8,27 +7,86 @@ import { Button, StyleSheet, Text, View } from 'react-native';
 
 const Stack = createStackNavigator();
 
+class CounterUp extends Component {
+
+  state = {count: 100}
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count - 1})
+    }, 1000)
+  }
+
+  render() {
+    const {count} = this.state
+    const {color, size} = this.props
+
+    return (
+      <Text style={{color, fontSize: size}}>
+        {count}
+      </Text>
+    )
+  }
+}
+
+
 function HomeScreen({ navigation }) {
   return (
+    <>
     <Button
-      title="Go to Jane's profile"
+      title="Go to the count up page.."
       onPress={() =>
-        navigation.navigate('Profile', { name: 'Jane' })
+        navigation.navigate('Counting up', { name: 'Counter' })
       }
     />
+    <View style={counterStyles.container}>
+    <CounterUp color={'lightblue'} size={16} />
+    <CounterUp color={'skyblue'} size={32} />
+    <CounterUp color={'steelblue'} size={80} />
+    <CounterUp color={'darkblue'} size={140} />
+  </View>
+  </>
   );
 }
 
+
+class Counter extends Component {
+  state = {count: 0}
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({count: this.state.count + 1})
+    }, 1000)
+  }
+
+  render() {
+    const {count} = this.state
+    const {color, size} = this.props
+
+    return (
+      <Text style={{color, fontSize: size}}>
+        {count}
+      </Text>
+    )
+  }
+}
 
 function DetailsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-    </View>
+    <View style={counterStyles.container}>
+    <Counter color={'lightblue'} size={16} />
+    <Counter color={'skyblue'} size={32} />
+    <Counter color={'steelblue'} size={80} />
+    <Counter color={'darkblue'} size={140} />
+  </View>
   );
 }
-
-
+const counterStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
 
 
 export default function App() {
@@ -36,20 +94,11 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen}
-          options={{ title: 'Welcome' }}
+          options={{ title: 'Welcome the counter App.' }}
         />
-        <Stack.Screen name="Profile" component={DetailsScreen} /> 
+        <Stack.Screen name="Counting up" component={DetailsScreen} /> 
       </Stack.Navigator>  
     </NavigationContainer>  
     )
-    // return <Main />
-  // return (
-  //   <View style={styles.container}>
-  //     <Text>Finn is stinky!</Text>
-  //     <Text>he is ugly</Text>
-  //     <Text>no</Text>
-  //     <Text>The quick brown fox jumps over the lazy dog over the banks of the river.</Text>
-  //   </View>
-  // );
 }
 
